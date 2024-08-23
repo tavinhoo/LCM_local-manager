bdf61c72585738311d4c7fe3ed472e674adf997a
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-08-23 10:34:36 -0300

    Primeiro commit

---
+++ src/main/java/com/localmanager/app/LocalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.localmanager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/localmanager/app/LocalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localmanager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
