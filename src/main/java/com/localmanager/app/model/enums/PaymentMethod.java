7d8d72beca90d1f0b5b65a2f93089e5466e5aac3
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-08-27 01:10:51 -0300

    Enums: payment status e payment method.

---
+++ src/main/java/com/localmanager/app/model/enums/PaymentMethod.java
@@ -0,0 +1,29 @@
+package com.localmanager.app.model.enums;
+
+public enum PaymentMethod {
+    DEBIT(1),
+    CREDIT(2),
+    PIX(3),
+    CASH(4),
+    PARTIAL(5),
+    NONE(6);
+
+    private int code;
+
+    private PaymentMethod(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public static PaymentMethod valueOf(int code) throws IllegalAccessException {
+        for(PaymentMethod value : PaymentMethod.values()) {
+            if(value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalAccessException("Método de pagamento não encontrado!");
+    }
+}
